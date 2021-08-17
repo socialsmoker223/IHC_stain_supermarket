123657ac7107dc1db231cd0fdf84066f92335dc0
tourist-C <ckto.curtis@gmail.com>
2021-08-17 17:08:00 +0800

    testing port number

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: sh setup.sh && python3 src/backend/main.py
+web: uvicorn main:app --host 0.0.0.0 --port $PORT
--- main.py
+++ main.py
@@ -8,19 +8,18 @@ import tempfile
 
 app = FastAPI()
 
-
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 @app.post("/train_rules")
 def train_rules(file: UploadFile = File(...)):
-
     trainer = Trainer()
     with tempfile.NamedTemporaryFile() as fp:
         fp.write(file.file.read())
         _, df_rules = trainer.fit(fp.name)
 
     return {"df": df_rules}
+
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=-5000)
+    uvicorn.run("main:app", host="0.0.0.0", port=5000)
