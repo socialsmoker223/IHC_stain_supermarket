f7328bd0813fa66e1fc46250022560c35ddab8e5
tourist-C <ckto.curtis@gmail.com>
2021-08-17 17:16:19 +0800

    test remove port number in main.py

--- main.py
+++ main.py
@@ -21,5 +21,5 @@ def train_rules(file: UploadFile = File(...)):
 
     return {"df": df_rules}
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=5000)
+# if __name__ == "__main__":
+#     uvicorn.run("main:app", host="0.0.0.0", port=5000)
