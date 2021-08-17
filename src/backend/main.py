f94cba334d2e4dfd02a119055c5cb74acaa16ba7
Joseph Choi <joseph2173@hotmail.com>
2021-08-17 16:29:14 +0800

    change port

--- src/backend/main.py
+++ src/backend/main.py
@@ -23,4 +23,4 @@ def train_rules(file: UploadFile = File(...)):
 
     return {"df": df_rules}
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8080)
+    uvicorn.run("main:app", host="0.0.0.0", port=-5000)
